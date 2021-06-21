cad9449ffc55eadcc67c05f48c7fd53b03eaba87
Vibha Verma <vibha.verma0209@gmail.com>
2021-06-21 22:43:39 +0530

    Initial commit

---
+++ contact.php
@@ -0,0 +1,449 @@
+<?php 
+$name_of_your_site = "yourcompanyname.com"; 
+$email_adress_reciever = "gsrthemes9@gmail.com";
+
+if(isset($_POST['Send']))
+{
+	include('send.php');	
+}
+?>
+<!doctype html>
+<!--[if IE 7 ]>    <html lang="en-gb" class="isie ie7 oldie no-js"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en-gb" class="isie ie8 oldie no-js"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="en-gb" class="isie ie9 no-js"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> <html lang="en-gb" class="no-js"> <!--<![endif]-->
+
+<head>
+    <title>Credit Repair : Southern California | 30 Day Credit Repair, phone: 1-888-928-5721</title>
+    
+    <meta charset="utf-8">
+    <meta name="keywords" content="Credit Repair, Credit Report, Consolidation Loans, Credit Repair Companies, Credit Consolidation, Bad Credit, Credit Repair Services, How To Rebuild Credit, Improve Credit Score, Fix My Credit, Credit Restoration, Slow Credit, Credit Help, Credit Restoration Service, Financial Service, Save Money, Free Credit Score" />
+    <meta name="description" content="Are You In Need of Credit Repair? 30 Day Credit Repair provides credit repair services in Southern California. Delete Bad Credit and Raise Your Credit Score, phone: 1-888-928-5721" />
+    
+    <!-- Favicon --> 
+    <link rel="shortcut icon" href="images/favicon.ico">
+    
+    <!-- this styles only adds some repairs on idevices  -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    
+    <!-- Google fonts - witch you want to use - (rest you can just remove) -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700' rel='stylesheet' type='text/css'>
+    
+    <!--[if lt IE 9]>
+        <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    
+    <!-- ######### CSS STYLES ######### -->
+    
+    <link rel="stylesheet" href="css/reset.css" type="text/css" />
+    <link rel="stylesheet" href="css/style.css" type="text/css" />
+    
+    <!-- responsive devices styles -->
+    <link rel="stylesheet" media="screen" href="css/responsive-leyouts.css" type="text/css" />
+
+    <!-- ######### FONT AWESOME ######### -->
+    <--<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
+<!-- just remove the below comments witch color skin you want to use -->
+    <!--<link rel="stylesheet" href="css/colors/lightblue.css" />-->
+    <!--<link rel="stylesheet" href="css/colors/lightgreen.css" />-->
+    <!--<link rel="stylesheet" href="css/colors/blue.css" />-->
+    <!--<link rel="stylesheet" href="css/colors/green.css" />-->
+    <!--<link rel="stylesheet" href="css/colors/red.css" />-->
+    <!--<link rel="stylesheet" href="css/colors/cyan.css" />-->
+    <!--<link rel="stylesheet" href="css/colors/purple.css" />-->
+    <!--<link rel="stylesheet" href="css/colors/yellow.css" />-->
+    <!--<link rel="stylesheet" href="css/colors/brown.css" />-->
+    
+<!-- just remove the below comments witch bg patterns you want to use --> 
+    <!--<link rel="stylesheet" href="css/bg-patterns/pattern-default.css" />-->
+    <!--<link rel="stylesheet" href="css/bg-patterns/pattern-one.css" />-->
+    <!--<link rel="stylesheet" href="css/bg-patterns/pattern-two.css" />-->
+    <!--<link rel="stylesheet" href="css/bg-patterns/pattern-three.css" />-->
+    <!--<link rel="stylesheet" href="css/bg-patterns/pattern-four.css" />-->
+    <!--<link rel="stylesheet" href="css/bg-patterns/pattern-five.css" />-->
+    <!--<link rel="stylesheet" href="css/bg-patterns/pattern-six.css" />-->
+    <!--<link rel="stylesheet" href="css/bg-patterns/pattern-seven.css" />-->
+    <!--<link rel="stylesheet" href="css/bg-patterns/pattern-eight.css" />-->
+    <!--<link rel="stylesheet" href="css/bg-patterns/pattern-nine.css" />-->
+    
+    <!-- style switcher -->
+    <link rel = "stylesheet" media = "screen" href = "js/style-switcher/color-switcher.css" />
+    
+    <!-- REVOLUTION SLIDER -->
+    <link rel="stylesheet" type="text/css" href="js/revolutionslider/css/fullwidth.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="js/revolutionslider/rs-plugin/css/settings.css" media="screen" />
+    
+    <!-- jquery jcarousel -->
+    <link rel="stylesheet" type="text/css" href="js/jcarousel/skin.css" />
+    <link rel="stylesheet" type="text/css" href="js/jcarousel/skin2.css" />
+    
+    <!-- thumbs zoom -->
+    <link rel="stylesheet" href="js/thumbszoom/thumbzoom.css" type="text/css" media="all">
+ 
+    
+</head>
+
+<body>
+
+    <div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<div class="site_wrapper">
+    <div class="container_full">
+        <div class="menu_sticky">
+            <div class="top-border-line"></div>
+                <div class="top_section">
+                    <div class="container">
+                        <div class="one_fourth">
+                            <div id="logo">
+                                <a href="index.html"><img width="100%" height="auto" src="images/logo.png">
+                                <strong>
+                                    <em>"We Start Challenging Your Negative Marks Immediately"</em></strong></a>
+                            </div>
+                        </div><!-- end logo -->
+
+                    <div class="three_fourth last">
+                        <div class="top_contact_info">
+                            <ul>
+                                <li><div class="hb-labels hb-labels-en " ><a href="#" class="button_header">FREE CONSULTATION</a>
+                                <div class="top_con">(888)928-5721</div> 
+                                <div class="top_con_l2">SERVING NATIONWIDE</div>
+                                </div></li>
+                                <li> <div class="hd-labels hd-labels-en "></div></li>
+                                
+                            </ul>
+                        </div><!-- end top contact info -->
+                    </div>
+
+                    <!--<div class="three_fourth last">
+                        <div class="">
+                            <div class="hd-labels hd-labels-en" style="height: 106px; margin: 45px 0px 0px; width: 210px;"></div>
+
+                        </div>
+                    </div>-->
+                </div>
+            </div><!-- end top section-->
+
+            <div class="menu_full">
+                <div class="container">
+                    <nav id="access" class="access" role="navigation">
+                        <div id="menu" class="menu">
+                            <ul id="tiny">
+                                <li><a href="index.html" >Home</a> </li>  
+                                <li><a href="hiw.html">How It Works</a> </li>
+                                <li><a href="services.html">Our Services</a>  </li>
+                                <li><a href="faqs.html">FAQ's</a> </li>
+                                <li><a href="result.html">Our Results</a>  </li>
+                                <li><a href="about.html" >About Us</a>  </li>
+                                <li><a href="contact.php" class="active">Contact</a>  </li>
+                            </ul>
+                        </div>
+                    </nav><!-- end nav menu -->
+                </div> 
+            </div>
+        </div>        
+      
+
+
+<div class="clearfix"></div>
+ 
+<div class="page_title">
+
+	<div class="container">
+		<div class="title"><h1>Contact Us</h1></div>
+        <div class="pagenation"><a href="index.html">Home</a> <i>/</i> Contact Us</div>
+	</div>
+</div><!-- end page title --> 
+
+<div class="clearfix"></div>   
+ 
+
+<!-- Contant
+======================================= -->
+ <div class="sec_pic2">
+
+<div class="container">
+
+	<div class="content_left">
+        	
+    <div class="one_half">
+        
+
+      
+    <p>Be sure to visit our Frequently Asked Questions page before you complete the form below to
+obtain answers to common questions such as pricing.</p>
+
+    <form action="" method="post">
+    
+        <fieldset>
+        
+        <?php if (!isset($errorC) || $errorC == true){ ?>
+        
+        <label for="name" class="blocklabel">Your Name*</label>
+        <p class="<?php if (isset($the_nameclass)) echo $the_nameclass; ?>" ><input name="yourname" class="input_bg" type="text" id="name" value=''/></p>
+        
+        
+        <label for="email" class="blocklabel">E-Mail*</label>
+        <p class="<?php if (isset($the_emailclass)) echo $the_emailclass; ?>" ><input name="email" class="input_bg" type="text" id="email" value='' /></p>
+        
+        
+        <label for="phone" class="blocklabel">Phone</label>
+        <p><input name="phone" class="input_bg" type="text" id="phone" value=""/></p>
+        
+        <label for="bestway" class="blocklabel">Best Way to Contact</label>
+        <p><input name="bestway" class="input_bg" type="text" id="bestway" value=""/></p>
+        
+        <label for="phone" class="blocklabel">State</label>
+        <p><input name="state" class="input_bg" type="text" id="state" value=""/></p>
+        
+        <label for="help" class="blocklabel">I need help with:</label>
+        <div name="help" class="check_bg" id="help" >
+        <input type="radio" name="g" value="Home Loan">Home Loan<br>
+            <input type="radio" name="g" value="Refinancing">Refinancing<br>
+            <input type="radio" name="g" value="Auto Loan">Auto Loan<br>
+            <input type="radio" name="g" value="Improving My Credit Score">Improving My Credit Score</div>
+        
+        <label for="message" class="blocklabel">Message*</label>
+        <p class="<?php if (isset($the_messageclass)) echo $the_messageclass; ?>"><textarea name="message" class="textarea_bg" id="message" cols="20" rows="7" ></textarea></p>
+        
+        
+        <p>
+        <input type="hidden" id="myemail" name="myemail" value="<?php echo $email_adress_reciever; ?>" />
+        <input type="hidden" id="myblogname" name="myblogname" value="<?php echo $name_of_your_site; ?>" />
+        <div class="clearfix"></div>
+        <input name="Send" type="submit" value="SUBMIT" class="comment_submit" id="send"/></p>
+        <?php } else { ?> 
+        
+        <!--<div class="success">
+            <div class="message-box-wrap">
+               Your message has been <strong>Received Successfully!</strong> Thank you!</div>
+        </div>-->
+        
+        <?php } ?>
+        
+        </fieldset>
+        
+        </form> 
+    
+    </div>
+               
+    <div class="one_half last">
+    
+        <div class="address-info">
+            <h3>CORPORATE HEADQUARTERS</h3>
+                <ul>
+                <li>
+                <strong>30 Day Credit Repair</strong><br />
+                16349 Harbor Blvd,<br /> Fountain Valley CA 92708<br />
+               
+                </li>
+                <li>
+                <strong>HOURS OF OPERATION</strong><br />
+               Monday Sunday<br />
+               9:30am 6:30pm PST<br>
+               Including Holidays
+                </li>
+                <li>
+                <strong>Phone Numbers</strong><br />
+               Toll: 1-888-928-5721<br />
+               P: 714-531-3800<br>
+               F: 8772458977
+                </li>
+            </ul>
+
+        </div>
+
+
+         <h3>Find the Address</h3>
+            <iframe class="google-map" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=WA,+United+States&amp;aq=0&amp;oq=WA&amp;sll=47.605288,-122.329296&amp;sspn=0.008999,0.016544&amp;ie=UTF8&amp;hq=&amp;hnear=Washington,+District+of+Columbia&amp;t=m&amp;z=7&amp;iwloc=A&amp;output=embed"></iframe><br /><small><a href="http://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=WA,+United+States&amp;aq=0&amp;oq=WA&amp;sll=47.605288,-122.329296&amp;sspn=0.008999,0.016544&amp;ie=UTF8&amp;hq=&amp;hnear=Washington,+District+of+Columbia&amp;t=m&amp;z=7&amp;iwloc=A">View Larger Map</a></small>
+        
+    </div>
+            
+</div>
+ <div class="right_sidebar">
+
+    <div class="sidebar_widget" style="text-align:center;">
+        <div><img src="images/banner01.png" width="100%" height="auto"></div>
+       <br>
+        <div class="sidetext"><img src="images/quot3.png" width="20px" height="20px">&nbsp;&nbsp;Wow ...<br>in 30 short days of their diligent work, my credit score jumped over 100 points!  -- George &nbsp;&nbsp;<img src="images/quot4.png" width="20px" height="20px"><br><br><br><br><br></div>
+           <div ><a href="contact.php" class="button_header">free to start!...</a></div><br>
+     
+                                
+    </div>
+        <div class="clearfix mar_top3"></div>
+            
+    </div><!-- end rightsidebar -->
+
+</div><!-- end main content area -->
+</div>
+
+
+
+<div class="clearfix bottom_buildings"></div><!-- end buildings graph -->
+
+
+<!-- Footer
+======================================= -->
+
+<div id="footer">
+
+<div class="footer_columns">
+
+    <div class="container">
+    
+        <div class="one_fourth">
+        
+            <h2><i>useful</i> links</h2>
+            
+            <ul class="arrows_list1">
+            <li><a href="aboutcredit.html">All About The Credit</a></li>
+            <li><a href="Glossary.html">Glossary</a></li>
+            <li><a href="http://creditrepairpartner.offerit.com/external.php?page=index" target="_blank">Become An Affiliate</a></li>
+            <li><a href="BOpportunity.html">Business Opportunity</a></li>
+            <li><a href="PPolicy.html">Privacy Policy</a></li>
+            <li><a href="TUse.html">Terms Of Use</a></li>
+            <li><a href="Sitemap.html">Sitemap</a></li>
+            </ul>
+             
+        </div><!-- end section --> 
+        
+        <div class="one_fourth">
+        
+          <div class="fb-like-box" data-href="https://www.facebook.com/pages/30-Day-Credit-Repair/410792105709063" data-width="400" data-colorscheme="dark" data-show-faces="true" data-header="true" data-stream="false" data-show-border="true" style="float:right;"></div>   
+        </div>       
+        <div class="one_fourth last">
+        
+            <h2><i>About</i> 30 Day Credit</h2>
+            
+            <div class="newsletter">
+            <p>30 Day Credit Repair was establishedto help consumers gain a better understanding about the importance of good credit.</p>
+           
+             <div class="rss_feeds"><a href="#">Read More</a></div>
+             
+            </div>
+        
+        </div><!-- end section -->
+
+</div>
+  
+</div><!-- end footer all columns --> 
+
+<div class="clearfix divider_line5"></div>
+
+<div class="container">
+    <div class="scrollup_area"><a href="#" class="scrollup">i</a></div><!-- end scroll to top of the page-->
+</div>
+
+<div class="copyright_info">
+
+    <div class="container">
+    
+        <div class="one_third">
+        
+            <div class="footer_logo"><a href="index.html"><img width="100%" height="auto" src="images/logo.png"></div><!-- end logo -->
+            <b>Copyright © 2014 30 Day Credit Repair. All Rights Reserved.</b>
+            
+        </div>
+         <div class="one_third">
+        
+            <div ><img width="100%" height="auto" src="images/blank.png"></div><!-- end logo -->
+        </div>
+    
+        <div class="one_fourth last">
+            
+                <div class="address_info">
+            
+                30 Day Credit Repair, Serving Nationwide<br />
+                Phone: +888 928 5721<br />
+                E-mail: <a href="mailto:repairingcredits@gmail.com">repairingcredits@gmail.com</a><br />
+            
+            </div>
+                
+        </div>
+    
+    </div>
+    
+</div><!-- end copyright info -->  
+
+</div>
+
+
+
+ 
+</div>
+
+    
+<!-- ######### JS FILES ######### -->
+<!-- get jQuery from the google apis -->
+<script type="text/javascript" src="js/universal/jquery.js"></script>
+
+<!-- style switcher -->
+<script src="js/style-switcher/jquery-1.js"></script>
+<script src="js/style-switcher/styleselector.js"></script>
+
+<!-- main menu -->
+<script type="text/javascript" src="js/mainmenu/ddsmoothmenu.js"></script>
+<script type="text/javascript" src="js/mainmenu/jquery-1.7.1.min.js"></script>
+<script type="text/javascript" src="js/mainmenu/selectnav.js"></script>
+
+<!-- jquery jcarousel -->
+<script type="text/javascript" src="js/jcarousel/jquery.jcarousel.min.js"></script>
+
+<script type="text/javascript" src="js/mainmenu/scripts.js"></script>
+
+<!-- top show hide plugin script-->
+<script src="js/show-hide-plugin/showHide.js" type="text/javascript"></script>
+
+<!-- scroll up -->
+<script type="text/javascript">
+    $(document).ready(function(){
+ 
+        $(window).scroll(function(){
+            if ($(this).scrollTop() > 100) {
+                $('.scrollup').fadeIn();
+            } else {
+                $('.scrollup').fadeOut();
+            }
+        });
+ 
+        $('.scrollup').click(function(){
+            $("html, body").animate({ scrollTop: 0 }, 500);
+            return false;
+        });
+ 
+    });
+</script>
+
+<!-- jquery jcarousel -->
+<script type="text/javascript">
+
+	jQuery(document).ready(function() {
+			jQuery('#mycarousel').jcarousel();
+	});
+	
+	jQuery(document).ready(function() {
+			jQuery('#mycarouseltwo').jcarousel();
+	});
+	
+	jQuery(document).ready(function() {
+			jQuery('#mycarouselthree').jcarousel();
+	});
+	
+	jQuery(document).ready(function() {
+			jQuery('#mycarouselfour').jcarousel();
+	});
+	
+</script>
+
+<!-- accordion -->
+<script type="text/javascript" src="js/accordion/custom.js"></script>
+
+
+</body>
+</html>
